3c90d6db55c65fdacadf48a4e253666a5df05d4d
Andrea Toffanello <andrea.toffanello@gmail.com>
2026-04-20 23:08:33 +0200

    refactor(ios): estrai PressableButtonStyle in file dedicato

--- ios/TransitKit/Sources/Views/Info/ServiceDetailView.swift
+++ ios/TransitKit/Sources/Views/Info/ServiceDetailView.swift
@@ -427,14 +427,3 @@ struct ServiceDetailView: View {
     }
 }
 
-// MARK: - PressableButtonStyle
-
-/// Pressed-state style for solid-fill buttons: light scale + opacity shift.
-struct PressableButtonStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .scaleEffect(configuration.isPressed ? 0.98 : 1.0)
-            .opacity(configuration.isPressed ? 0.92 : 1.0)
-            .animation(.spring(response: 0.25, dampingFraction: 0.8), value: configuration.isPressed)
-    }
-}
